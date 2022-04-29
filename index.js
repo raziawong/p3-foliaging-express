f7f51ed50ac3a95fc354dac7cf2d9c8f3e7a87b0
Razia <nrwpc.stack@gmail.com>
2022-04-29 20:55:18 +0800

    move middleware to middleware/index.js

--- index.js
+++ index.js
@@ -2,13 +2,16 @@ const express = require("express");
 const hbs = require("hbs");
 const waxOn = require("wax-on");
 const cors = require("cors");
-require("dotenv").config();
-
-const session = require("express-session");
 const flash = require("express-flash");
-const FileStore = require("session-file-store")(session);
-const csrf = require("csurf");
-const { variables } = require("./helpers/const");
+require("dotenv").config();
+const {
+  initSession,
+  setUser,
+  setCSRF,
+  setCSRFToken,
+  setFlashMessages,
+  handleErrors,
+} = require("./middleware");
 
 const app = express();
 app.set("view engine", "hbs");
@@ -24,46 +27,13 @@ app.use(
 );
 
 app.use(cors());
-
-app.use(
-  session({
-    store: new FileStore(),
-    secret: process.env.SESSION_SECRET_KEY,
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(function (req, res, next) {
-  res.locals.user = req.session.user;
-  next();
-});
-
-const csrfInstance = csrf();
-app.use(function (req, res, next) {
-  csrfInstance(req, res, next);
-});
-app.use(function (req, res, next) {
-  if (req.csrfToken) {
-    res.locals.csrfToken = req.csrfToken();
-  }
-  next();
-});
-
+app.use(initSession());
+app.use(setUser);
+app.use(setCSRF);
+app.use(setCSRFToken);
 app.use(flash());
-app.use(function (req, res, next) {
-  res.locals[variables.success] = req.flash(variables.success);
-  res.locals[variables.error] = req.flash(variables.error);
-  next();
-});
-
-app.use(function (err, req, res, next) {
-  if (err && err.code == "EBADCSRFTOKEN") {
-    req.flash(variables.error, "The form has expired. Please try again");
-    res.redirect("back");
-  } else {
-    next();
-  }
-});
+app.use(setFlashMessages);
+app.use(handleErrors);
 
 const ims = {
   specifications: require("./routes/pim/specifications"),
---
+++ middleware/index.js
@@ -0,0 +1,70 @@
+const session = require("express-session");
+const FileStore = require("session-file-store")(session);
+const csrf = require("csurf");
+const { variables } = require("../helpers/const");
+const csrfInstance = csrf();
+
+require("dotenv").config();
+
+const initSession = () => {
+  return session({
+    store: new FileStore(),
+    secret: process.env.SESSION_SECRET_KEY,
+    resave: false,
+    saveUninitialized: true,
+  });
+};
+
+const setUser = (req, res, next) => {
+  //res.app.locals.user = req.session.user;
+  res.locals.user = req.session.user;
+  next();
+};
+
+const setCSRF = (req, res, next) => {
+  csrfInstance(req, res, next);
+};
+
+const setCSRFToken = (req, res, next) => {
+  if (req.csrfToken) {
+    res.locals.csrfToken = req.csrfToken();
+  }
+  next();
+};
+
+const setFlashMessages = (req, res, next) => {
+  res.locals[variables.success] = req.flash(variables.success);
+  res.locals[variables.error] = req.flash(variables.error);
+  next();
+};
+
+const checkIfAuthenticated = (req, res, next) => {
+  if (req.session.user) {
+    next();
+  } else {
+    req.flash("error_messages", "You need to sign in to access this page");
+    res.redirect("/users/login");
+  }
+};
+
+const handleErrors = (err, req, res, next) => {
+  if (err && err.code == "EBADCSRFTOKEN") {
+    req.flash(variables.error, "The form has expired. Please try again");
+    res.redirect("back");
+  } else if (err) {
+    res.status(err.status || 500);
+    res.send(err);
+  } else {
+    next();
+  }
+};
+
+module.exports = {
+  initSession,
+  setUser,
+  setCSRF,
+  setCSRFToken,
+  setFlashMessages,
+  checkIfAuthenticated,
+  handleErrors,
+};
