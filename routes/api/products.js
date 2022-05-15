073fb1c8e9b2f4430f3a6c5938515327eb3ea1dd
Razia <nrwpc.stack@gmail.com>
2022-05-15 17:17:16 +0800

    add routes to separate product collections

--- routes/api/products.js
+++ routes/api/products.js
@@ -1,20 +1,61 @@
 const router = require("express").Router();
-const { getAllProducts } = require("../../database/access/products");
-const ImageServices = require("../../database/services/image-services");
+const { searchProducts } = require("../../database/access/products");
+const ProductServices = require("../../database/services/product-services");
 
-router.get("/", async (req, res) => {
-  let products = await getAllProducts();
+const searchAndProcessProducts = async (builder) => {
+  let products = await searchProducts(builder);
 
   if (products) {
     products = products.toJSON();
     for (const item of products) {
+      const productService = new ProductServices(item.id, item.discounts);
+
       if (item.uploadcare_group_id) {
-        item.images = await new ImageServices(item.id).getImagesUrls();
+        item.images = await productService.getImagesUrls();
       }
+
+      item.deals = productService.getDeals();
     }
   }
 
-  res.send(products);
+  return products;
+};
+
+router.get("/", async (req, res) => {
+  const queries = req.query;
+  const builder = (qb) => {};
+  const results = await searchAndProcessProducts(builder);
+  res.send(results);
+});
+
+router.get("/plants", async (req, res) => {
+  const queries = req.query;
+  const builder = (qb) => {
+    qb.whereNotNull("plant_id");
+  };
+
+  const results = await searchAndProcessProducts(builder);
+  res.send(results);
+});
+
+router.get("/planters", async (req, res) => {
+  const queries = req.query;
+  const builder = (qb) => {
+    qb.whereNotNull("planter_id");
+  };
+
+  const results = await searchAndProcessProducts(builder);
+  res.send(results);
+});
+
+router.get("/supplies", async (req, res) => {
+  const queries = req.query;
+  const builder = (qb) => {
+    qb.whereNotNull("supply_id");
+  };
+
+  const results = await searchAndProcessProducts(builder);
+  res.send(results);
 });
 
 module.exports = router;
