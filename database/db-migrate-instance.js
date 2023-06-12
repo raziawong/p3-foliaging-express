7bd79bb2cc9ede0a07112ca4833c2df30105bdd0
Razia <nrwpc.stack@gmail.com>
2023-06-12 15:59:48 +0800

    update env name

--- database/db-migrate-instance.js
+++ database/db-migrate-instance.js
@@ -9,7 +9,7 @@ const getDBM = () => {
         driver: process.env.DB_DRIVER,
         user: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB_NAME,
+        database: process.env.DB_DATABASE,
         host: process.env.DB_HOST,
         ssl: {
           rejectUnauthorized: false,
