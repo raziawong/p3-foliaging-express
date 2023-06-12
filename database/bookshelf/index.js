1f15c81afb6e777e4b70a168881795337a22bd6b
Razia <nrwpc.stack@gmail.com>
2023-06-12 23:17:57 +0800

    rename DB_NAME to DB_DATABASE

--- database/bookshelf/index.js
+++ database/bookshelf/index.js
@@ -3,7 +3,7 @@ const knex = require("knex")({
   connection: {
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    database: process.env.DB_DATABASE,
     host: process.env.DB_HOST,
     ssl: {
       rejectUnauthorized: false,
